4f6d3967800c18e7bf6cab5ff71f5377b838414c
Matt LeMay <mplemay@users.noreply.github.com>
2026-04-10 12:41:11 -0500

    fix: required tidyverse loading

---
+++ R/kelloggmktg482-package.R
@@ -0,0 +1,8 @@
+#' Package imports for kelloggmktg482
+#'
+#' @importFrom dplyr bind_cols bind_rows filter mutate
+#' @importFrom ggplot2 aes annotate coord_flip geom_col geom_hline geom_line geom_point geom_segment geom_tile ggplot labs position_dodge xlab ylab
+#' @importFrom magrittr %>%
+#' @importFrom tidyr gather
+#' @noRd
+NULL
--- R/update.R
+++ R/update.R
@@ -3,7 +3,9 @@
 #' This function updates the kelloggmkgt482 package
 #' @export
 #' @examples
+#' \dontrun{
 #' update_kelloggmkgt482()
+#' }
 update_kelloggmkgt482 <- function() {
   devtools::install_github("blakemcshane/kelloggmktg482", upgrade = "never", force = TRUE)
   }
